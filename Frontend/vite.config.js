da9d3570e10730535902ec49bfe6dd1a9a584710
Taradon63130500058 <taradon.job@mail.kmutt.ac.th>
2022-05-02 03:52:51 +0700

    [Update] frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
+  devserver: {
     proxy: {
       '/api' : {
         target: 'http://backendclinic',
