4d2bbfce6b7cc57685ecef3acb493b0660583ae8
teeradet63130500061 <teeradet.bigkung@mail.kmutt.ac.th>
2022-05-09 00:42:40 +0700

    [Add] Public Network

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,9 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-    //  http://intproj21.sit.kmutt.ac.th/
+  base: '/kw4',
+  server: {
+    proxy: {
+      '/api' : {
+        target: 'http://intproj21.sit.kmutt.ac.th',
+        // target: 'http://10.4.84.58:8080',
+        changeOrigin: true,
+        secure: false,
+        // transpileDependencies: true
+        // rewrite: (path) => path.replace(/^\/api/,'') 
+      }
     }
   }
 })
