caac2df2bc3051bb51516c56ab72004d757a45bb
teeradet63130500061 <teeradet.bigkung@mail.kmutt.ac.th>
2022-05-23 00:40:17 +0700

    [Update] Infra

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,17 +5,17 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/kw4',
-  server: {
-    proxy: {
-      '/api' : {
-        target: 'http://intproj21.sit.kmutt.ac.th',
-        // target: 'http://10.4.84.58:8080',
-        changeOrigin: true,
-        secure: false,
-        // transpileDependencies: true
-        // rewrite: (path) => path.replace(/^\/api/,'') 
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api' : {
+  //       target: 'http://intproj21.sit.kmutt.ac.th',
+  //       // target: 'http://10.4.84.58:8080',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       // transpileDependencies: true
+  //       // rewrite: (path) => path.replace(/^\/api/,'') 
+  //     }
+  //   }
+  // }
 })
 
