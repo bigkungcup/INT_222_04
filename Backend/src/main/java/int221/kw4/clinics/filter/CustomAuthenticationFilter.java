232736fe68bdae1a1eeb9ba161c6ccc2e247a632
teeradet63130500061 <teeradet.bigkung@mail.kmutt.ac.th>
2022-09-24 00:03:13 +0700

    [Update JWT Show Role]

--- Backend/src/main/java/int221/kw4/clinics/filter/CustomAuthenticationFilter.java
+++ Backend/src/main/java/int221/kw4/clinics/filter/CustomAuthenticationFilter.java
@@ -81,6 +81,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         Map<String, String> tokens = new HashMap<>();
         tokens.put("access_token", access_token);
         tokens.put("refresh_token", refresh_token);
+        tokens.put("role", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()).toString());
         response.setContentType(APPLICATION_JSON_VALUE);
         new ObjectMapper().writeValue(response.getOutputStream(),tokens);
     }
